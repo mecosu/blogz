f9a69b704ba7aa7ba9de3530d28e488c61f9d081
mecosu <30613825+mecosu@users.noreply.github.com>
2018-07-11 22:28:54 -0600

    created user class and db table

--- main.py
+++ main.py
@@ -17,15 +17,28 @@ class Blog(db.Model):
     body = db.Column(db.String)
     datetime = db.Column(db.DateTime)
     date = db.Column(db.Date)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         self.datetime = datetime.now()
         self.date = datetime.now().date()
+        self.owner = owner
 
     def __repr__(self):
         return '<Blog %r)>' % (self.title, self.body)
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 @app.route("/newpost", methods=['POST'])
 def add_new_post():
